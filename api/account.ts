51d3723adec99d634a2ad7e67bc79ea61c527bf0
Nathaniel Walston <nathaniel.walston@kareo.com>
2019-12-04 08:22:22 -0800

    Make request handlers as strait forward as possible

--- api/account.ts
+++ api/account.ts
@@ -6,6 +6,9 @@ import sql from "sql-template-strings";
 import { query } from "./_util/db-driver";
 import { manglePassword, newSalt } from "./_util/password";
 
+/** Known Errors */
+const DUPLICATE_ENTRY = Error("DUPLICATE ENTRY ON INSERT");
+
 /**
  * POST /account
  * X-FINGERPRINT='string'
@@ -28,33 +31,17 @@ import { manglePassword, newSalt } from "./_util/password";
  * ', [account_id, x_fingerprint, token])
  */
 
-const DUPLICATE_ENTRY = Error("DUPLICATE ENTRY");
-
 const app = new Koa();
 const router = new Router();
 router
   .use(bodyparser())
   .post("Create Account", "/account", async (ctx, next) => {
     const { account_name, password } = ctx.request.body;
-    const salt = newSalt();
-    const hashedPassword = manglePassword(password, salt);
-    const accountName = String(account_name).toLocaleLowerCase();
 
-    let uuid: any = "00000-00000-00000-00000-00000";
     try {
-      await query(sql`INSERT INTO "accounts"
-        ("id", "username", "hashed_password", "salt")
-        VALUES
-        (gen_random_uuid(), ${accountName}, ${hashedPassword}, ${salt})
-        RETURNING "accounts"."id";
-      `)
-        .then(rows => {
-          uuid = rows[0].id;
-        })
-        .catch(err => {
-          if (err.detail && err.detail.includes("already exists"))
-            throw DUPLICATE_ENTRY;
-        });
+      ctx.status = 200;
+      ctx.body = { uuid: await createAccount(account_name, password) };
+      return next();
     } catch (e) {
       if (e === DUPLICATE_ENTRY) {
         ctx.status = 409;
@@ -65,12 +52,37 @@ router
         return next();
       }
     }
-
-    ctx.status = 200;
-    ctx.body = { hashedPassword, accountName, salt, uuid };
+  })
+  .put("Update Password", "/account", async (ctx, next) => {
+    ctx.status = 430;
+    ctx.body = "Operation Not Yet Supported";
+    return next();
+  })
+  .delete("Remove Account", "/account", async (ctx, next) => {
+    ctx.status = 430;
+    ctx.body = "Operation Not Yet Supported";
     return next();
   });
 
 app.use(router.routes()).use(router.allowedMethods());
 
 export default app;
+
+function createAccount(accountName: string, password: string): Promise<string> {
+  accountName = String(accountName).toLocaleLowerCase();
+
+  const salt = newSalt();
+  const hashedPassword = manglePassword(password, salt);
+
+  return query(sql`INSERT INTO "accounts"
+    ("id", "username", "hashed_password", "salt")
+    VALUES
+    (gen_random_uuid(), ${accountName}, ${hashedPassword}, ${salt})
+    RETURNING "accounts"."id";
+  `)
+    .then(rows => rows[0].id)
+    .catch(err => {
+      if (err.detail && err.detail.includes("already exists"))
+        throw DUPLICATE_ENTRY;
+    });
+}
